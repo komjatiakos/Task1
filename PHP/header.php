06747680cc94cae801dc376ef497cd53dcb87b7f
Ákos Komjáti <komjati.akos@outlook.com>
2020-10-20 19:15:34 +0200

    Tweak

--- PHP/header.php
+++ PHP/header.php
@@ -1,15 +1,15 @@
 <nav class="navbar navbar-expand-sm navbar-dark bg-dark">
-    <a href="http://localhost/task1/VIEW/index.php" class="navbar-brand">Komjáti Ákos</a>
+    <a href="./../VIEW/index.php" class="navbar-brand">Komjáti Ákos</a>
     <button class="navbar-toggler" data-toggle="collapse" data-target="#navbarMenu">
         <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarMenu">
         <ul class="navbar-nav">
             <li class="nav-item">
-                <a href="http://localhost/task1/VIEW/add.php" class="nav-link">Listázás</a>
+                <a href="./../VIEW/add.php" class="nav-link">Listázás</a>
             </li>
             <li class="nav-item">
-                <a href="http://localhost/task1/VIEW/list.php" class="nav-link">Hozzáadás</a>
+                <a href="./../VIEW/list.php" class="nav-link">Hozzáadás</a>
             </li>
         </ul>
     </div>
